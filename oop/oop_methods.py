f4519780a37843ccbab5bdae704618cfb06b278e
Adrian Gonciarz <adrian.gonciarz@appdate.pl>
2019-09-19 22:57:34 +0200

    some basics

---
+++ oop/oop_methods.py
@@ -0,0 +1,20 @@
+# Writing methods
+def sing_a_song():
+    print(
+        """A 60 ton angel falls to the earth
+A pile of old metal, a radiant blur
+Scars in the country, the summer and her"""
+    )
+
+def read_a_poem():
+    print("""Once upon a midnight dreary, while I pondered, weak and weary,
+Over many a quaint and curious volume of forgotten lore—
+    While I nodded, nearly napping, suddenly there came a tapping,
+As of some one gently rapping, rapping at my chamber door.
+“’Tis some visitor,” I muttered, “tapping at my chamber door—
+            Only this and nothing more.”""")
+
+# main method
+if __name__ == '__main__':
+    sing_a_song()
+    read_a_poem()
