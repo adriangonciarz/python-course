be47fcaf6ddfeaaabe98f79cc2937c0909cff178
Adrian Gonciarz <adrian.gonciarz@synerise.com>
2019-09-24 07:19:27 +0200

    add lambda filter example

--- oop/oop_methods_tricks.py
+++ oop/oop_methods_tricks.py
@@ -21,6 +21,10 @@ def descriptive_function(**kwargs):
     print(f'First name: {kwargs["firstname"]}, last name: {kwargs["lastname"]}')
 
 
+def filter_divisive_by_three(numbers):
+    return list(filter(lambda n: n % 3 == 0, numbers))
+
+
 if __name__ == '__main__':
     print(a())
     print(b)
@@ -28,3 +32,6 @@ if __name__ == '__main__':
 
     elastic_function('cat', 'cow', 1975)
     descriptive_function(firstname='James', lastname='Brown')
+    original_list = [1, 3, 13, 14, 16, 18, 21, 190, 303]
+    filtered_list = filter_divisive_by_three(original_list)
+    print(filtered_list)
