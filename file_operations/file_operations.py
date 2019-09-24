ddd66dddba89a0b223c1bb051ed1386cfe15216a
Adrian Gonciarz <adrian.gonciarz@synerise.com>
2019-09-24 07:10:38 +0200

    file operations on JSON

--- file_operations/file_operations.py
+++ file_operations/file_operations.py
@@ -1,4 +1,7 @@
 # Classical path
+import json
+import pprint
+
 f1 = open('classical_file.txt', 'w')
 f1.write('String one\n')
 f1.write('String two\n')
@@ -16,3 +19,18 @@ with open('animals.csv', 'r') as animals_file:
     del lines[0]  # drop headers
     for l in lines:
         print(l.strip('\n'))
+
+with open('addresses.json', 'r') as json_file:
+    js = json_file.read()
+    js_dict = json.loads(js)
+    pprint.pprint(js_dict)
+
+
+def echo():
+    content = input('Provide some string to echo:\n')
+    print(content)
+
+
+if __name__ == '__main__':
+    while True:
+        echo()
